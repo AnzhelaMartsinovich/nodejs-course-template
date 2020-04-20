02ea501676a221ce14d1fa3ab67c4c57b1334c68
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2020-04-20 17:56:55 +0300

    refactor: code

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -1,26 +1,26 @@
 const router = require('express').Router();
 const Board = require('./board.model');
-const boardService = require('./board.service');
+const { getAll, getById, create, update } = require('./board.service');
 
 router.route('/').get(async (req, res) => {
-  const allBoards = await boardService.getAll();
+  const allBoards = await getAll();
   res.json(allBoards.map(board => Board.toResponse(board)));
 });
 
 router.route('/:id').get(async (req, res) => {
   const id = req.params.id;
-  res.json(Board.toResponse(await boardService.getById(id)));
+  res.json(Board.toResponse(await getById(id)));
 });
 
 router.route('/').post(async (req, res) => {
   const body = req.body;
-  res.json(Board.toResponse(await boardService.create(body)));
+  res.json(Board.toResponse(await create(body)));
 });
 
 router.route('/:id').put(async (req, res) => {
   const id = req.params.id;
   const body = req.body;
-  res.json(Board.toResponse(await boardService.update(id, body)));
+  res.json(Board.toResponse(await update(id, body)));
 });
 
 module.exports = router;
