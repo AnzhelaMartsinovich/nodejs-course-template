8879fe3ae3c93435566317db7b51a317c27f71c2
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2020-04-20 17:47:46 +0300

    refactor: clean db.client

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('./../resources/users/user.model');
 const { MONGO_CONNECTION_STRING } = require('./../common/config');
-const usersJSON = require('./../data/users.json');
-
-const users = usersJSON.map(user => new User(user));
 
 const connectToDB = cb => {
   mongoose.connect(MONGO_CONNECTION_STRING, {
@@ -16,7 +12,6 @@ const connectToDB = cb => {
   db.once('open', () => {
     console.log('we are connected!');
     db.dropDatabase();
-    users.forEach(user => user.save());
     cb();
   });
 };
